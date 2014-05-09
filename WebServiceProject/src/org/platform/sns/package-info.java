3a9985428d54c27e6da9c96998ab09793f364027
Marco Cavallo <mar86.cvl@gmail.com>
2014-05-09 16:41:23 +0200

    Deleted old files

--- WebServiceProject/src/org/platform/sns/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://interfaces.base.snps.osgi.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.platform.sns;
